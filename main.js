05ac1a04e5d835ac286dff384c6459172f5b7cc9
Ahmed Abuelgasim <27422810+a-abuelgasim@users.noreply.github.com>
2025-02-10 17:41:38 -0500

    global changes needed to prepare for v3 reskin

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
 import './sass/style.scss';
 
+
+const YEARS_PLACEHOLDER = '%years';
+
+
+function getYearsOfExperience() {
+  const givenDate = new Date('2016-04-01');
+  const today = new Date();
+
+  const years = today.getFullYear() - givenDate.getFullYear();
+  if (
+    today.getMonth() < givenDate.getMonth() ||
+    (today.getMonth() === givenDate.getMonth() && today.getDate() < givenDate.getDate())
+  ) {
+    return years - 1;
+  }
+
+  return years;
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
+	// Keep years of experience updated
+	document
+		.querySelectorAll('[replace-exp-years]')
+		.forEach((elem) => {
+			const newTextContent = elem.textContent.replace(YEARS_PLACEHOLDER, getYearsOfExperience());
+			elem.textContent = newTextContent;
+		});
+
 	// Prevent flash of unstyled content (FOUC)
 	document.getElementById('main').style.display = '';
-
-	// Keep 'years of experience' in profile updated
-	document
-		.getElementById('years')
-		.textContent = new Date().getFullYear() - 2016;
 });
